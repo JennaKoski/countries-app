cfea80b48cdcedc523797c0fbd4238b35193490a
JennaKoski <jenna.m.koski@gmail.com>
2022-09-05 11:23:48 +0300

    redux and toolkit installed, base files created

---
+++ src/features/countries/countriesSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const countriesSlice = createSlice({
+  name: "countries",
+  initialState: {
+    countries: [],
+  },
+  reducers: {},
+});
+
+// export const {} = countriesSlice.actions;
+
+export default countriesSlice.reducer;
